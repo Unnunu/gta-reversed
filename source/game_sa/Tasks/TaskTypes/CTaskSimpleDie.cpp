2d7db8d25f0365468eaccd4860ecb8f36d896d01
Unnunu <serge_zorkin@mail.ru>
2021-07-11 14:59:37 +0300

    reverse constructors and destructor

--- source/game_sa/Tasks/TaskTypes/CTaskSimpleDie.cpp
+++ source/game_sa/Tasks/TaskTypes/CTaskSimpleDie.cpp
@@ -1,1 +1,68 @@
 #include "StdInc.h"
+
+// 0x62FA00
+CTaskSimpleDie::CTaskSimpleDie(eAnimGroup nAnimGroup, eAnimID nAnimID, float fBlendDelta, float fAnimSpeed)
+{
+    m_nAnimGroup = nAnimGroup;
+    m_nAnimID = nAnimID;
+    m_fBlendDelta = fBlendDelta;
+    m_fAnimSpeed = fAnimSpeed;
+    b1 = false;
+    b2 = false;
+    m_pAnimHierarchy = nullptr;
+    m_nAnimFlags = 0;
+    m_pAnim = nullptr;
+}
+
+// 0x62FA60
+CTaskSimpleDie::CTaskSimpleDie(const char* animName, const char* animBlockName, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed)
+{
+    m_nAnimGroup = ANIM_GROUP_DEFAULT;
+    m_nAnimID = ANIM_ID_KO_SHOT_FRONT_0;
+    m_fBlendDelta = fBlendDelta;
+    m_fAnimSpeed = fAnimSpeed;
+    b1 = false;
+    b2 = false;
+    m_pAnimHierarchy = CAnimManager::GetAnimation(animName, CAnimManager::GetAnimationBlock(animBlockName));
+    m_nAnimFlags = nAnimFlags;
+    m_pAnim = nullptr;
+}
+
+// 0x62FAF0
+CTaskSimpleDie::CTaskSimpleDie(CAnimBlendHierarchy* pAnimHierarchy, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed)
+{
+    m_nAnimGroup = ANIM_GROUP_DEFAULT;
+    m_nAnimID = ANIM_ID_KO_SHOT_FRONT_0;
+    m_fBlendDelta = fBlendDelta;
+    m_fAnimSpeed = fAnimSpeed;
+    b1 = false;
+    b2 = false;
+    m_pAnimHierarchy = pAnimHierarchy;
+    m_nAnimFlags = nAnimFlags;
+    m_pAnim = nullptr;
+}
+
+CTaskSimpleDie* CTaskSimpleDie::Constructor(eAnimGroup nAnimGroup, eAnimID nAnimID, float fBlendDelta, float fAnimSpeed)
+{
+    this->CTaskSimpleDie::CTaskSimpleDie(nAnimGroup, nAnimID, fBlendDelta, fAnimSpeed);
+    return this;
+}
+
+CTaskSimpleDie* CTaskSimpleDie::Constructor2(const char* animName, const char* animBlockName, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed)
+{
+    this->CTaskSimpleDie::CTaskSimpleDie(animName, animBlockName, nAnimFlags, fBlendDelta, fAnimSpeed);
+    return this;
+}
+
+CTaskSimpleDie* CTaskSimpleDie::Constructor3(CAnimBlendHierarchy* pAnimHierarchy, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed)
+{
+    this->CTaskSimpleDie::CTaskSimpleDie(pAnimHierarchy, nAnimFlags, fBlendDelta, fAnimSpeed);
+    return this;
+}
+
+// 0x62FB40
+CTaskSimpleDie::~CTaskSimpleDie()
+{
+    if (m_pAnim)
+        m_pAnim->SetFinishCallback(CDefaultAnimCallback::DefaultAnimCB, nullptr);
+}
--- source/game_sa/Tasks/TaskTypes/CTaskSimpleDie.h
+++ source/game_sa/Tasks/TaskTypes/CTaskSimpleDie.h
@@ -1,21 +1,38 @@
 #include "CTaskSimple.h"
 
-class CAnimBlendHierarchy;
-class CAnimBlendAssociation;
+//class CAnimBlendHierarchy;
+//class CAnimBlendAssociation;
 
 class CTaskSimpleDie : public CTaskSimple
 {
-    std::int32_t m_animGroupId;
-    std::int32_t m_animId;
-    CAnimBlendHierarchy* m_animHierarchy;
-    std::uint32_t m_animFlags;
-    float m_blendDelta;
-    float m_animSpeed;
-    std::uint8_t m_flags;
+    eAnimGroup m_nAnimGroup;
+    eAnimID m_nAnimID;
+    CAnimBlendHierarchy* m_pAnimHierarchy;
+    unsigned int m_nAnimFlags;
+    float m_fBlendDelta;
+    float m_fAnimSpeed;
+    union {
+        unsigned char m_nFlags;
+        struct
+        {
+            unsigned char b1 : 1;
+            unsigned char b2 : 1;
+        };
+    };
+    char _pad[3];
+    CAnimBlendAssociation* m_pAnim;
+
+    CTaskSimpleDie(eAnimGroup nAnimGroup, eAnimID nAnimID, float fBlendDelta, float fAnimSpeed);
+    CTaskSimpleDie(const char* animName, const char* animBlockName, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed);
+    CTaskSimpleDie(CAnimBlendHierarchy* pAnimHierarchy, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed);
+
 private:
-    std::uint8_t padding[3];
+    CTaskSimpleDie* Constructor(eAnimGroup nAnimGroup, eAnimID nAnimID, float fBlendDelta, float fAnimSpeed);
+    CTaskSimpleDie* Constructor2(const char* animName, const char* animBlockName, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed);
+    CTaskSimpleDie* Constructor3(CAnimBlendHierarchy* pAnimHierarchy, unsigned int nAnimFlags, float fBlendDelta, float fAnimSpeed);
+
 public:
-    CAnimBlendAssociation* m_animAssociation;
+    ~CTaskSimpleDie() override;
 };
 
 VALIDATE_SIZE(CTaskSimpleDie, 0x28);
